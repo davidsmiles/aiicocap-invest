b14b643e50d5f7718bac78abc7620f655e29962b
davidsmiles <ugberodavid@gmail.com>
2019-11-04 14:15:25 +0100

    use httpconnection to make post request

---
+++ app/src/main/java/com/app/aiicapinvest/API.kt
@@ -0,0 +1,70 @@
+package com.app.aiicapinvest
+
+import java.io.*
+import java.net.HttpURLConnection
+import java.net.URL
+import java.nio.charset.Charset
+
+
+object API {
+
+    private const val URL = "https://aiicocap-restapi.herokuapp.com"
+
+    fun signup() = "${URL}/"
+
+    fun signin(user_id: String, password: String) = "${URL}action=signin&user_id=$user_id&password=$password"
+
+    fun makeApiCall(url: URL, jsonToString: String = ""): String? {
+        var jsonResponse: String? = null
+        var urlConnection: HttpURLConnection? = null
+        var inputStream: InputStream? = null
+        var outputStream: OutputStream? = null
+
+        try {
+            urlConnection = url.openConnection() as HttpURLConnection
+            urlConnection.apply {
+                requestMethod = "GET"
+            //    setRequestProperty("Content-Type", "application/json; utf-8")
+            //    setRequestProperty("Accept", "application/json")
+                connectTimeout = 10000
+                doOutput = true
+                connect()
+            }
+
+            outputStream = urlConnection.outputStream
+            inputStream = urlConnection.inputStream
+
+        //    writeToStream(outputStream, jsonToString)
+            jsonResponse = readFromStream(inputStream)
+
+        } catch (e: IOException) { }
+        finally {
+            urlConnection?.disconnect()
+            if (inputStream != null) {
+                try {
+                    inputStream.close()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+
+            }
+        }
+
+        return jsonResponse
+    }
+
+    private fun writeToStream(outputStream: OutputStream?, jsonToString: String){
+        val input = jsonToString.toByteArray(Charset.forName("utf-8"))
+        outputStream!!.write(input, 0, input.size)
+    }
+
+    @Throws(IOException::class)
+    private fun readFromStream(inputStream: InputStream?): String {
+
+        val inputStreamReader = InputStreamReader(inputStream, Charset.forName("UTF-8"))
+        val reader = BufferedReader(inputStreamReader)
+
+        return reader.readText()
+    }
+
+}
--- app/src/main/java/com/app/aiicapinvest/SignUp.kt
+++ app/src/main/java/com/app/aiicapinvest/SignUp.kt
@@ -1,14 +1,22 @@
 package com.app.aiicapinvest
 
 
+import android.app.AlertDialog
+import android.content.Context
+import android.os.AsyncTask
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import dmax.dialog.SpotsDialog
 import kotlinx.android.synthetic.main.fragment_sign_up.*
+import java.net.MalformedURLException
+import java.net.URL
+import java.util.*
 
 
 class SignUp : Fragment(), View.OnClickListener {
@@ -27,12 +35,72 @@ class SignUp : Fragment(), View.OnClickListener {
 
         navController = Navigation.findNavController(view)
 
+        sign_up.setOnClickListener(this)
         login.setOnClickListener(this)
     }
 
     override fun onClick(view: View?) {
         when(view!!.id){
+            sign_up.id -> {
+                Toast.makeText(context, "signup clicked", Toast.LENGTH_SHORT).show()
+                val jsonInputString = "{'firstname': 'David'}"
+                Signup(context!!).execute(jsonInputString)
+            }
             login.id -> navController.navigate(R.id.action_signUp_to_login)
+        }
+    }
+
+    internal inner class Signup(var ctx: Context) : AsyncTask<String, Void, String>() {
+
+        lateinit var dialog: AlertDialog
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+
+            dialog = SpotsDialog.Builder()
+                .setContext(ctx)
+                .setMessage(String.format(Locale.getDefault(), "Processing..."))
+                .setCancelable(false)
+                .setTheme(R.style.Custom)
+                .build()
+        }
+
+        override fun doInBackground(vararg json: String): String? {
+
+            val signup = API.signup()
+
+            var url: URL? = null
+
+            try {
+                url = URL(signup)
+            } catch (e: MalformedURLException) {
+                e.printStackTrace()
+            }
+
+            publishProgress()
+
+            return API.makeApiCall(url!!, json[0])
+        }
+
+        override fun onProgressUpdate(vararg values: Void?) {
+            super.onProgressUpdate(*values)
+
+            dialog.show()
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+
+            dialog.dismiss()
+
+            Toast.makeText(ctx, result, Toast.LENGTH_SHORT).show()
+
+//            if (result != null) {
+//                if(result.contains("true")){
+//                    val welcome = WelcomeSheet()
+//                    welcome.show(supportFragmentManager, "example")
+//                }
+//            }
         }
     }
 }
