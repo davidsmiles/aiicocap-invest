a3b799c9ac75f98162dae3653ddad61d2012deaf
davidsmiles <ugberodavid@gmail.com>
2019-11-13 07:54:02 +0100

    load user info into profile fragment

--- app/src/main/java/com/app/aiicapinvest/ProfileFragment.kt
+++ app/src/main/java/com/app/aiicapinvest/ProfileFragment.kt
@@ -2,10 +2,16 @@ package com.app.aiicapinvest
 
 
 import android.os.Bundle
+import android.text.Editable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_profile.*
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
 
 
 class ProfileFragment : Fragment() {
@@ -18,5 +24,26 @@ class ProfileFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        loadUserInfo()
+    }
 
+    private fun loadUserInfo(){
+        var data: String? = null
+        context!!.openFileInput("user_data.txt").use{
+            val ins = InputStreamReader(it)
+            val bis = BufferedReader(ins)
+            data = bis.readText()
+        }
+
+        JSONObject(data!!).also {
+            val firstname = it.getString("firstname")
+            val lastname = it.getString("lastname")
+            val email = it.getString("email")
+
+            fn.setText(firstname)
+            ln.setText(lastname)
+            em.setText(email)
+        }
+    }
 }
